1958b9402a6fd12b415e646acb59e7da5c4d3b49
mamank_gamink <akuntmbl922@gmail.com>
2026-06-14 20:08:32 +0800

    Initial commit

---
+++ api/dim_keterangan.js
@@ -0,0 +1,6 @@
+import { tables } from '../lib/tables.js';
+import { handleCrud } from '../lib/crud.js';
+
+export default function handler(req, res) {
+  return handleCrud(req, res, tables.dim_keterangan);
+}
